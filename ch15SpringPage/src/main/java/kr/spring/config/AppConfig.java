7dda58d20a31b1cc015fd9aee634181fce546c80
user <user@DESKTOP-LC52269>
2024-06-26 18:49:42 +0900

    asgqge

---
+++ ch15SpringPage/src/main/java/kr/spring/config/AppConfig.java
@@ -0,0 +1,59 @@
+package kr.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
+
+import kr.spring.interceptor.loginCheckInterceptor;
+
+//자바코드 기반 설정 클래스
+@Configuration
+public class AppConfig implements WebMvcConfigurer{
+	private loginCheckInterceptor loginCheck;
+	
+	
+	@Bean
+	public loginCheckInterceptor interceptor2() {
+		loginCheck = new loginCheckInterceptor();
+		return loginCheck;
+	}  
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		//LoginCheckInterceptor 설정 
+		registry.addInterceptor(loginCheck).addPathPatterns("/member/myPage")
+											.addPathPatterns("/member/update");
+		
+	}
+	
+	@Bean
+	public TilesConfigurer tilesConfigurer() {
+		final TilesConfigurer configurer = 
+				                  new TilesConfigurer();
+		//XML 설정 파일 경로 지정
+		configurer.setDefinitions(new String[] {
+				"/WEB-INF/tiles-def/main.xml",
+				"/WEB-INF/tiles-def/member.xml"
+		});
+		configurer.setCheckRefresh(true);
+		return configurer; 
+	}
+	@Bean
+	public TilesViewResolver tilesViewResolver() {
+		final TilesViewResolver tilesViewResolver = 
+				                  new TilesViewResolver();
+		tilesViewResolver.setViewClass(TilesView.class);
+		return tilesViewResolver;
+	}
+}
+
+
+
+
+
+
+
